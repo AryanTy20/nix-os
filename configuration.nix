e324edb324a6d97fdc7bc107ba473fd439ad3352
Aryan Tirkey <devty007@gmail.com>
2024-12-25 23:13:44 +0530

    refactor: config file

--- configuration.nix
+++ configuration.nix
@@ -1,64 +1,73 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# Main NixOS Configuration File
+# This file defines system-wide settings and the packages to be installed.
+# For detailed guidance, refer to the NixOS manual and 'configuration.nix(5)' man page.
 
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-  # Bootloader.
+  ##################
+  # System Bootloader
+  ##################
+  # Configures the bootloader (systemd-boot in this case) and EFI variables.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  ##################
+  # Networking Configuration
+  ##################
+  # Define the system's hostname
+  networking.hostName = "nixos";
 
-  # Configure network proxy if necessary
+  # Enable NetworkManager for managing networking connections
+  networking.networkmanager.enable = true;
+
+  # Configure network proxy if necessary (uncomment and edit as required)
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
+  ##################
+  # Time and Localization Settings
+  ##################
+  # Set the system time zone
   time.timeZone = "Asia/Kolkata";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_IN";
-
+  # Localization settings to ensure UTF-8 encoding
+  i18n.defaultLocale = "en_IN.UTF-8";  # Set default locale with UTF-8
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN";
-    LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
+    LC_ADDRESS = "en_IN.UTF-8";
+    LC_IDENTIFICATION = "en_IN.UTF-8";
+    LC_MEASUREMENT = "en_IN.UTF-8";
+    LC_MONETARY = "en_IN.UTF-8";
+    LC_NAME = "en_IN.UTF-8";
+    LC_NUMERIC = "en_IN.UTF-8";
+    LC_PAPER = "en_IN.UTF-8";
+    LC_TELEPHONE = "en_IN.UTF-8";
+    LC_TIME = "en_IN.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  ##################
+  # X11 and Desktop Environment
+  ##################
+  # Enable the X11 windowing system
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
+  # Enable GNOME Desktop Environment with the GDM display manager
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
+  # Configure the keymap for X11 (use the US layout)
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Enable CUPS to print documents.
+  ##################
+  # Printing and Sound Configuration
+  ##################
+  # Enable CUPS for printing services
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # Configure sound using PipeWire (disable PulseAudio)
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -66,34 +75,29 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # Enable JACK support if needed (uncomment to enable)
+    # jack.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  ##################
+  # User Configuration and System Packages
+  ##################
+  # Define user accounts and groups
   users.users.aryan = {
     isNormalUser = true;
     description = "aryan";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" ];  # Add to wheel for sudo privileges
     packages = with pkgs; [
-    #  thunderbird
+      # Add more user-specific packages here
+      # thunderbird
     ];
   };
 
-  # Install firefox.
+  # Enable Firefox browser for all users
   programs.firefox.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # Install system-wide packages
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
     curl
@@ -102,36 +106,29 @@
     vscode
   ];
 
+  # Allow unfree packages (e.g., for Visual Studio Code)
   nixpkgs.config.allowUnfreePredicate =
     pkg: builtins.elem (pkgs.lib.getName pkg) [
-     "vscode"
+      "vscode"
     ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
+  ##################
+  # Services Configuration
+  ##################
+  # Uncomment to enable OpenSSH for remote access
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
+  # Open specific firewall ports if required (uncomment and configure as needed)
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
+  
+  # Disable the firewall (not recommended unless necessary)
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  ##################
+  # System Versioning
+  ##################
+  # Set the system state version (leave at the default for your NixOS version)
+  system.stateVersion = "24.11";  # Ensure compatibility with your NixOS release
 
 }
